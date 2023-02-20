d63b759cdeb9c7af45e4cdeadf340eed575a0a07
Te1man1 <gmlg4u@gmail.com>
2023-02-20 21:41:08 +0300

    big commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import logo from './logo.svg';
+import Form from './form';
+import cloudy from './images/35.svg';
+import React, { useState, setState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a>NOW ILL COMMIT AND ANOTHER COMMIT</a>
-      </header>
+class App extends React.Component {
+  state = {
+    temp: undefined,
+    city: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.searchcity.value; 
+    const api_url = await
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&exclude=daily&appid=279feed19b448f72ecef1af05aa0bcdc&units=metric`);
+    const weatherdata = await api_url.json();
+    console.log(weatherdata);
+
+    this.setState({
+      temp: weatherdata.main.temp,
+      feelslike: weatherdata.main.feels_like,
+      windspeed: weatherdata.wind.speed,
+      pressure: weatherdata.main.pressure,
+      lon: weatherdata.coord.lon,
+      lat: weatherdata.coord.lat,
+      city: weatherdata.name,
+      error: " "
+    });
+  }
+
+  render(){
+    return (
+
+    <div className='Main'>
+      <Form weatherMethod = {this.getWeather}/>
+      <div className="InfoWidgets">
+       <div className="Info">
+           <div className="Days">
+           <div className='Today'>
+             <div className='TodayCity'>{this.state.city}</div>
+             <div className='TodayLine'></div>
+             <img className='Todayimg' src={cloudy} /> 
+             <div className='TodayTemp'>{this.state.temp}°C</div>
+             <div className='TodayFeelslike'>Real feel {this.state.feelslike}°</div>
+             <div className='TodayWind'>Wind {this.state.windspeed}m/s </div>
+             <div className='TodayPressure'>Pressuree {this.state.pressure}hPa</div>
+             
+           </div>
+            <div className='Day2'>
+              <p></p>
+            </div>
+            <div className='Day3'>
+              <p></p>
+            </div>
+            <div className='Day4'>
+              <p></p>
+            </div>
+            <div className='Day5'>
+              <p></p>
+            </div>
+          </div>
+          <div className="Map"> 
+           <div className='ymaps-layers-pane' id='Yandex-map'></div>
+          </div>
+        </div>
+        <div className="Widgets">
+          <div className="Wind-status">
+            <p></p>
+          </div>
+          <div className="Sun-position">
+            <p></p>
+          </div>
+          <div className="UV-index">
+            <p></p>
+          </div>
+         </div>
+      </div>
     </div>
-  );
+    );  
+  }
 }
 
 export default App;
---
+++ src/form.js
@@ -0,0 +1,19 @@
+import React from "react";
+import search from './images/search-icon.webp'
+
+import './App.css';
+
+class Form extends React.Component {
+    render(){
+        return(
+            <form onSubmit={this.props.weatherMethod} className="Header">       
+            <img className='Search-img' src={search} alt="Search" /> 
+            <input type="text" name='searchcity' placeholder='Search' className='Search-text'>
+            </input>
+        </form>
+
+        );
+    }
+}
+
+export default Form;
